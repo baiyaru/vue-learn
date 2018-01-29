05fa73c8b604cb9825d1994d1d5ff6514a84758b
xiaobai <1143486861@qq.com>
2018-01-29 17:26:20 +0800

    添加设置

--- src/pages/Setting.vue
+++ src/pages/Setting.vue
@@ -1,55 +1,75 @@
-<!-- 模板：html结构 -->
 <template>
-  <div>
+  <div class="user-info">
     <mt-header fixed :title="title" class="title">
       <router-link :to="{path: '/', meta: {tab: 'tab4'}}" slot="left">
         <mt-button icon="back"></mt-button>
-        <!--<mt-button @click="handleClose">我</mt-button>-->
       </router-link>
     </mt-header>
-    <div class="">
-      <div class="list">
-        <mt-cell title="账户与安全" is-link ></mt-cell>
-      </div>
-      <div class="list">
-        <mt-cell title="实名认证" is-link ></mt-cell>
-        <mt-cell title="车主认证" is-link ></mt-cell>
-      </div>
-      <div class="list">
-        <mt-cell title="我的地址" is-link ></mt-cell>
-      </div>
-      <div class="list">
-        <mt-cell title="法律条款" is-link ></mt-cell>
-        <mt-cell title="关于我们" is-link ></mt-cell>
-      </div>
+    <div class="list">
+      <mt-cell title="账户安全" is-link ></mt-cell>
+    </div>
+    <div class="list">
+      <mt-cell title="实名认证" is-link ></mt-cell>
+      <mt-cell title="车主认证" is-link ></mt-cell>
+    </div>
+    <div class="list">
+      <mt-cell title="我的地址" is-link ></mt-cell>
+    </div>
+    <div class="list">
+      <mt-cell title="法律条款" is-link ></mt-cell>
+      <mt-cell title="关于我们" is-link ></mt-cell>
+    </div>
+    <div class="list">
+      <button class="mint-button mint-button--default mint-button--large" @click = "test"><!---->
+        <label class="mint-button-text">退出登录</label>
+      </button>
     </div>
   </div>
 </template>
 
-<!-- 行为：处理逻辑-->
 <script>
   import { MessageBox } from 'mint-ui';
-
+  import '../assets/styles/page/user-info.scss'
   export default {
     name: "Setting",
     data() {
       return {
-        title: "設置"
+        title: "设置"
+      }
+    },methods:{
+      test(){
+        MessageBox({
+          title: '您确定退出？',
+          message: '退出将会删除您的个人数据。',
+          showCancelButton: true
+        });
       }
     }
   }
 
 
+
+
 </script>
 
-<!-- 样式：界面样式 -->
 <style scoped>
+  .user-info {
+    margin-top: 44px;
+  }
 
   .title {
     height: 44px;
     font-size: 18px;
     color: #fff;
     background-color: #333;
+  }
+
+  .list {
+    margin: 12px 0;
+  }
+
+  .mint-button-text {
+    color: #5bafff;
   }
 
 </style>
